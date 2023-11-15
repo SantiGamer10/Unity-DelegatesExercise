c7ca67e9b406ae504fbe6dafb2e2db64b04ae240
SantiGamer10 <150269675+SantiGamer10@users.noreply.github.com>
2023-11-15 11:01:23 -0500

    Update DataConsumer.cs

--- Assets/Scripts/Exercise1-Delegates/DataConsumption/DataConsumer.cs
+++ Assets/Scripts/Exercise1-Delegates/DataConsumption/DataConsumer.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class DataConsumer : MonoBehaviour
 {
-    //TODO: Complete this method
-    public void ReportPersonData(int personId
+    //TODO (DONE): Complete this method
+    public void ReportPersonData(int personId, Func<int, string> getName, Func<int, int> getAge, Func<int, short> getPostalCode
                                 /*, Receive the method getName()*/
                                 /*, Receive the method getAge()*/
                                 /*, Receive the method GetPostalCode*/)
@@ -15,11 +15,11 @@ public class DataConsumer : MonoBehaviour
         int age = -1;
         short postalCode = -1;
         
-        /*
-         * name = getName(personId);
-         * age = getAge(personId);
-         * postalCode = getPostalCode(personId);
-         */
+        
+         name = getName(personId);
+         age = getAge(personId);
+         postalCode = getPostalCode(personId);
+        
         
         var dataReport = string.Format("name: {0}, age: {1}, postal code: {2}", name, age, postalCode);
         Debug.Log($"{name}: Giving report on Data. {dataReport}");
